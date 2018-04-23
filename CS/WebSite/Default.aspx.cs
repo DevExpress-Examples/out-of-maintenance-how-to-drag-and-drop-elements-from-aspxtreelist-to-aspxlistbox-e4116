38551bed78cdfd7473d915566f5e955ec8edf755
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxTreeList;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Init(object sender, EventArgs e) {
+    }
+    protected void ASPxTreeList1_CustomCallback(object sender, TreeListCustomCallbackEventArgs e) {
+        ASPxTreeList tree = sender as ASPxTreeList;
+        TreeListNode deletingNode = tree.FindNodeByKeyValue(e.Argument);
+        DeleteNodeAndChildren(deletingNode,tree);        
+    }
+    private void DeleteNodeAndChildren(TreeListNode deletingNode, ASPxTreeList tree) {
+        if(deletingNode.HasChildren) {
+            foreach(TreeListNode child in deletingNode.ChildNodes) {
+                DeleteNodeAndChildren(child,tree);
+            }
+        }        
+        //tree.DeleteNode(deletingNode.Key); //Uncoment this line to allow node deleting
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to drag-and-drop elements from ASPxTreeList to ASPxListBox
+
+
+<p>This example will show how to drag a node to ASPxListBox and delete it and it's children from ASPxTreeList after this.<br />
+Uncomment the last line in the DeleteNodeAndChildren method to allow deleting a node from the datasource.</p>
+
+<br/>
+
+
